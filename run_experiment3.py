dd99d98aa776ba9de06d9a3acd55002f589aa08e
Nathan Mankovich <mankovic@kick.katrina.colostate.edu>
2021-08-30 16:56:09 -0600

    cnn practice

---
+++ run_experiment3.py
@@ -0,0 +1,32 @@
+import train_model as tm 
+from torchvision import models, transforms
+from torch import optim
+import torch 
+import os
+import my_models as mm
+
+
+datapath = '/data4/mankovic/FRD-NN/raw_datasets/mnist_splits/all_train_test_split/'
+
+base_dir = '/data4/mankovic/FRD-NN/experiments/mnist/raw/cnn/rotation_augmentation/raw_triangle_nn_3layer_lr_p1/'
+
+# os.mkdir(base_dir)
+# os.mkdir(base_dir + 'model')
+
+save_model_location = base_dir +'model/model'
+save_csv_location = base_dir +'training_stats.csv'
+
+
+
+batch_size=64
+
+
+data_transforms = mm.choose_transforms('raw_nn_RRC')
+
+model = mm.choose_model('raw_cnn_1')
+
+opt = optim.Adadelta(model.parameters(), lr=.1)
+
+# nSamples = list(dict(Counter(image_dataset['train'].targets)).values())
+# weights = torch.tensor([1 - (x / sum(nSamples)) for x in nSamples]).to(device)
+tm.train_model(datapath, model, data_transforms, opt, save_model_location, save_csv_location, 20, batch_size, frd_normalize = True)
