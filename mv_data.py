63868180d7c15113f171efc3d4aca7908d400a3e
Nathan Mankovich <mankovic@kick.katrina.colostate.edu>
2021-09-07 10:30:35 -0600

    check

---
+++ mv_data.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+shutil.move('./experiments', '/data4/mankovic/FRD-NN/experiments')
+
+shutil.move('./FRD_datasets', '/data4/mankovic/FRD-NN/FRD_datasets')
+
+shutil.move('./raw_datasets', '/data4/mankovic/FRD-NN/eaw_datasets')
